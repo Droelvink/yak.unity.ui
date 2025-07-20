aabe99dbf8a2015e5ef2a29a4f7feac0050e551a
D Roelvink <dennis.roelvink@gmail.com>
2025-07-20 23:53:53 +0200

    Editor

---
+++ Runtime/Editor/SmartButtonEditor.cs
@@ -0,0 +1,26 @@
+using UnityEditor;
+
+[CustomEditor(typeof(SmartButton), true)]
+[CanEditMultipleObjects]
+public class SmartButtonEditor : Editor
+{
+    private SerializedProperty _interactableProp;
+    private SerializedProperty _navigationProp;
+    private SerializedProperty _onClickProp;
+
+    void OnEnable()
+    {
+        _interactableProp = serializedObject.FindProperty("m_Interactable");
+        _navigationProp = serializedObject.FindProperty("m_Navigation");
+        _onClickProp = serializedObject.FindProperty("m_OnClick");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(_interactableProp);
+        EditorGUILayout.PropertyField(_navigationProp);
+        EditorGUILayout.PropertyField(_onClickProp);
+        serializedObject.ApplyModifiedProperties();
+    }
+}
