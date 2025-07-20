1c679e31e320266c742f0235bb90ebb82163f5ca
D Roelvink <dennis.roelvink@gmail.com>
2025-07-21 00:10:04 +0200

    fixed controller click bug

--- Runtime/Scripts/YakButton/SmartButton.cs
+++ Runtime/Scripts/YakButton/SmartButton.cs
@@ -69,6 +69,7 @@ namespace com.yak.ui
         
         public override void OnSubmit(BaseEventData eventData)
         {
+            base.OnSubmit(eventData);
             _pressing = true;
             ApplyEffects();
             StartCoroutine(ControllerPressEvent());
